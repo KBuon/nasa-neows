51340cae09bb593d3a8248989aa417706d0ecaa5
Kuntharith Buon <kuntharith.buon@gmail.com>
2016-09-12 20:45:07 -0400

    Renamed index.js to main.js, finalizing for v1.0.1

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-// ============================================================================
-// Author: Kuntharith Buon
-// Description: Node.js wrapper for NASA's Near Earth Object Web Service (NeoWS)
-// ============================================================================
-
 // Dependency
 var request = require('request');
 
-// Errors
-var callbackUndefinedError = new Error("Callback function is not defined.");
-var invalidDateType = new TypeError("endDate must be of type string");
-var invalidDatePatternError = new Error("Please use following format for date: yyyy-mm-dd");
-var invalidAsteroidType = new TypeError("asteroid_id must be of type string");
+//Errors
+var undefApiKey = new Error("'api_key' is not defined");
+var undefCallback = new Error("'callback' function is not defined");
+var undefOptions = new Error("'options' is not defined");
+var undefDetailed = new Error("'isDetailed' is not defined");
+var undefAsteroidID = new Error("'asteroid_id' is not defined");
 
+var apiKeyType = new TypeError("'api_key' must be of type string");
+var invalidCallbackType = new TypeError("'callback' must be a function");
+var invalidOptionsType = new TypeError("'options' must be of type object");
+var invalidDetailedType = new TypeError("'isDetailed' must be of type boolean");
+var invalidAsteroidType = new TypeError("'asteroid_id' must be of type string");
+
+// Endpoints 
 var baseURL = "https://api.nasa.gov/neo/rest/v1/";
 var endpoints = {
 	'feed' : baseURL + 'feed?',
@@ -21,114 +24,164 @@ var endpoints = {
 	'asteroid' : baseURL + '/neo/'
 };
 
-// Default Parameters
-var parameters = {
-	'detailed' : 'true',
-	'api_key' : 'DEMO_KEY'
+var client  = function(api_key) {
+	if (api_key == undefined){
+		throw undefApiKey;
+		return;
+	} else if (typeof api_key !== 'string'){
+		throw apiKeyType;
+		return;
+	}
+
+	this.api_key = api_key;
 };
+/*
+	var options = {
+		'start_date': 'xyz',
+		'end_date' : 'xyz',
+		'isDetailed' : true | false
+	};
+*/
+client.prototype.getFeed = function(options, callback){
+	// Error checking
+	if (options == undefined){
+		throw undefOptions;
+		return;
+	} else if (typeof options !== 'object'){
+		throw invalidOptionsType;
+		return;
+	}
 
-// Public  
-module.exports = {
-	// Requires end_date (string) argument
-	getFeed : function(startDate, endDate, callback) {
-		if (endDate == undefined || typeof endDate !== 'string'){
-			throw invalidDateType;
-			return;
-		}
+	if (callback == undefined){
+		throw undefCallback;
+		return;
+	} else if (typeof callback !== 'function'){
+		throw invalidCallbackType;
+		return;
+	}
 
-		if(!/^\d{4}\-\d{1,2}\-\d{1,2}$/.test(endDate)){
-			throw invalidDatePatternError;
-			return;
-		}
-		var params;
+	var params = {};
 
-		if (startDate != undefined){
-			params = { 
-				'start_date' : startDate,
-				'end_date' : endDate,
-				'detailed' : 'true',
-				'api_key' : 'DEMO_KEY'
-			};
+	for (var x in options){
+		if (options[x] != null){
+			params[x] = options[x];
 		}
-		else {
-			 params = {
-				'end_date' : endDate,
-				'detailed' : 'true',
-				'api_key' : 'DEMO_KEY'
-			};
-		}
-		request({url : endpoints.feed,
-			 qs: params,
-			 json: true}, function(error, response, data) {
-				callback (error, data);
-			 });
-	},
-	getFeedToday : function(params, callback) {
-		params = (params == undefined || typeof params === 'undefined') ? parameters : params;
-
-		// Error Check: callback must be defined
-		if (callback == undefined || typeof callback !== 'function') {
-			throw callbackUndefinedError;
-			return;
-		}
-
-		request({url : endpoints.feedToday,
-			 qs : parameters,
-			 json : true}, function (error, response, data) {
-				callback(error, data);
-			 });
-	},
-	getStats : function(callback) {
-		// Error Check: callback must be defined
-		if (callback == undefined || typeof callback != 'function'){
-			throw callbackUndefinedError;
-			return;
-		}
-		
-		var params = {
-			'api_key' : parameters.api_key
-		};
-
-		request({url : endpoints.stats, qs: params, json : true}, function (error,
-		response, data) {
+	}
+	params["api_key"] = this.api_key;
+	
+	request({url: endpoints.feed, qs: params, json: true},
+		function(error, response, data){
 			callback(error, data);
 		});
-	},
-	// Utilize default parameters if user does not define
-	browse : function(params, callback) {
-		params = (params == undefined || typeof params === 'undefined') ? { 
-			'page' : '0',
-			'size' : '20',
-			'api_key' : parameters.api_key
-		} : params;
-
-		// Error Check : callback must be defined
-		if (callback == undefined || typeof callback !== 'function') {
-			throw callbackUndefinedError;
-			return;
-		}
-
-		request({url : endpoints.browse, 
-			 qs : params,
-			 json: true}, function (error, response, data) {
-				callback(error, data);
-			 });
-	},
-	// Requires asteroid_id (string) argument
-	getAsteroid : function(asteroid_id, callback) {
-		if (asteroid_id == undefined || typeof asteroid_id !== 'string'){
-			throw invalidAsteroidType;
-			return;
-		}
-		var url = endpoints.asteroid + asteroid_id + '?';
-		var params = {
-			'api_key' : 'DEMO_KEY'
-		};
-
-		request({url: url,
-			 qs: params,
-			 json : true}, function (error, response, data) {
-				callback(error, data);
-			 });
-	}
 };
+
+client.prototype.getFeedToday  = function(isDetailed, callback){
+	// Error checking
+	if (isDetailed == undefined){
+		throw undefDetailed;
+		return;
+	} else if (typeof isDetailed !== 'boolean'){
+		throw invalidDetailedType;
+		return;
+	}
+
+	if (callback == undefined){
+		throw undefCallback;
+		return;
+	} else if (typeof callback !== 'function'){
+		throw invalidCallbackType;
+		return;
+	}
+
+	var params = {
+		'detailed' : isDetailed.toString().toLowerCase(),
+		'api_key' : this.api_key
+	};
+
+	request({url: endpoints.feedToday, qs: params, json: true},
+		function(error, response, data){
+			callback(error, data);
+		});
+};
+
+client.prototype.getStats = function(callback){
+	// Error checking
+	if (callback == undefined){
+		throw undefCallback;
+		return;
+	} else if (typeof callback !== 'function'){
+		throw invalidCallbackType;
+		return;
+	}
+
+	var params = { 
+		'api_key' : this.api_key
+	};
+	
+	request({url: endpoints.stats, qs: params, json: true},
+		function(error, response, data){
+			callback(error, data);
+		});
+};
+
+client.prototype.browse = function(options, callback){
+	// Error checking
+	if (options == undefined){
+		throw undefOptions;
+		return;
+	} else if (typeof options !== 'object'){
+		throw invalidOptionsType;
+		return;
+	}
+
+	if (callback == undefined){
+		throw undefCallback;
+		return;
+	} else if (typeof callback !== 'function'){
+		throw invalidCallbackType;
+		return;
+	}
+
+	var params = {};
+
+	for (var x in options){
+		params[x] = options[x];
+	}
+	params["api_key"] = this.api_key;
+
+	request({url: endpoints.browse, qs: params, json:true}, 
+		function(error, response, data){
+			callback(error, data);
+		});
+};
+
+client.prototype.getAsteroid = function(asteroid_id, callback){
+	// Error checking
+	if (asteroid_id == undefined){
+		throw undefAsteroidID;
+		return;
+	} else if (typeof asteroid_id !== 'string'){
+		throw invalidAsteroidType;
+		return;
+	}
+
+	if (callback == undefined){
+		throw undefCallback;
+		return;
+	} else if (typeof callback !== 'function'){
+		throw invalidCallbackType;
+		return;
+	}
+
+	var url = endpoints.asteroid + asteroid_id + '?';
+	var params = {
+		'api_key' : this.api_key
+	};
+
+	request({url:url, qs: params, json: true},
+		function(error, response, data){
+			callback(error, data);
+		});
+};
+
+module.exports = client;
